1269b53bffad04c37f367b7f648c6be0d54df5a4
Nathan R. Liu <nathanliu17@gmail.com>
2022-04-02 23:21:32 -0700

    owner can list new tokens, people can buy

--- contracts/funkykeys.sol
+++ contracts/funkykeys.sol
@@ -5,28 +5,49 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract FunkyKeys is ERC1155{
 		
-	address private governance;
+	mapping(address => bool) private governance;
+	address tokenInventory;
 	uint256 private productCount;
+	mapping(uint256 => uint256) productToPrice;
 	
 	event tokenBurned(address indexed _sender, uint256 indexed _tokenID, uint16 _amount);
 
-	constructor() public ERC1155("") {
-		governance = msg.sender;
+	constructor(address _tokenInventory) public ERC1155("") {
+		governance[msg.sender] = true;
+		tokenInventory = _tokenInventory;
 		productCount = 0;
 	}
 
 	modifier ensureCallerIsOwner() {
-		require(governance == msg.sender, "Caller is not governance.");
+		require(governance[msg.sender], "Caller is not governance.");
 		_;
 	}
 
-	function addNewProduct(uint256 initialSupply) external ensureCallerIsOwner {
+	function addOwner(address _newOwner) external ensureCallerIsOwner{
+		governance[_newOwner] = true;
+	}
+
+	function addNewProduct(uint256 initialSupply, uint256 price) external ensureCallerIsOwner {
+		_mint(tokenInventory, productCount, initialSupply, "");
+		productToPrice[productCount] = price;
 		productCount++;
-		_mint(msg.sender, productCount, initialSupply, "");
+	}
+
+	function editPrice(uint256 price, uint256 _id) external ensureCallerIsOwner {
+		productToPrice[_id] = price;
+	}
+	
+	function getPrice(uint256 _id) public view returns (uint256){
+		return productToPrice[_id];
 	}
 	
 	function burn(uint256 _id, uint16 _count) public{
 		_burn(msg.sender, _id, _count);
 		emit tokenBurned(msg.sender, _id, _count);
+	}
+
+	function buyListing(uint256 _tokenID) public payable{
+		require (msg.value == productToPrice[_tokenID] * (1 ether), "Send the exact amount specified by getPrice");
+		_safeTransferFrom(tokenInventory, msg.sender, _tokenID, 1, "");
 	}
 }
